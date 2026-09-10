622904694000ea7fa570a38bf6a69de07560e2e3
pranjal yadav <yashydv0677@gmail.com>
2026-09-10 23:44:13 +0530

    lecture12_restapi

---
+++ lecture12_restapi.js
@@ -0,0 +1,16 @@
+// api provide the best practice that can be follow by us 
+//  it work on server cliet architecture
+//  response can be send in json format(raw data send kr diya client json read kraga and khud ki screen pa rendor kr lega
+/*
+client can be depend on server
+
+if we know that the client is browser than ot is good practice to send the data in form of html
+because it save time  AND HAVE 1 STEP1 lessor than json
+
+all respect all http method => jiska jo kama ha usko vohi krna do (get put patch post dlt);
+
+RESTFULL API => 1) SERVER CLLIENT ARTITECTURE
+2) RESPEECT HTTP FUNCTION
+
+
+*/
