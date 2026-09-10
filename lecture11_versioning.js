8b04fc679f4163624deadb46b4eac4a32e3c28af
pranjal yadav <yashydv0677@gmail.com>
2026-09-10 23:43:30 +0530

    lecture11_versioning

---
+++ lecture11_versioning.js
@@ -0,0 +1,22 @@
+// important for security
+
+/*
+
+"express": "^5.2.1",
+1st part =5;
+2nd part =2
+3rd part =1;
+//  3 part => minor fixes(optional)
+//  2 part => recommended bug fix(security fix);
+//  1 part => major/ breaking release (existiong code may not run in next gen if already written in prev gen)
+its is not recomended to change the gen in existiong code  
+
+^ symbol => compatable with version (uss number ko lock kr do usko change mat kro
+1 part ka aaga lagta ha means 2 3 part can be change but 1 part can not change )
+
+~ symbol =>  only the 3 part change (1 and 2 part can not change(minor fix))
+
+we also can give the range of update 
+
+if we are careless regards to the version it may be possibel that your website can be hack 
+*/
