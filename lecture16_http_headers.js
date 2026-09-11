34c8784e454d364935eb8737f5c06e5950f1a798
pranjal yadav <yashydv0677@gmail.com>
2026-09-11 22:54:15 +0530

    lecture16_http_headers

---
+++ lecture16_http_headers.js
@@ -0,0 +1,13 @@
+/*  jab bi hum req ya respomse krta ha to ek packet banta ha information ka usma 
+like an envolop  the actual information is inside it but there is enformation also on the envolop 
+that is the header
+
+http headers are an importanat part of the api request and 
+res as they represent the meat data accosited with the api
+req &res
+meta data => data of data
+headers carry information for thr req and res body
+ 
+res header 
+req header
+ */
