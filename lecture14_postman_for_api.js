31f2df57c67a0de28423d2413d66710634c2ec4a
pranjal yadav <yashydv0677@gmail.com>
2026-09-11 22:49:36 +0530

    lecture14_postman_for_api

---
+++ lecture14_postman_for_api.js
@@ -0,0 +1,9 @@
+// BEST TO USE HEPLS IN TO TEST THE API'S  (GET ,POST ,PORT,DELTE  )
+// error come because express does not know which type of data os this and how to handle it 
+// so we have to use the middleware(just assume it is a plug in)
+
+// we have  fetch fs  (reason pta krna ha )
+
+/*
+  
+*/
