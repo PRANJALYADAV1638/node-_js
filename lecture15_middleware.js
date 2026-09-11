78a732d929eb224fe0d28a9ec47ded3b6d4ea0e4
pranjal yadav <yashydv0677@gmail.com>
2026-09-11 22:50:32 +0530

    lecture15_middleware

---
+++ lecture15_middleware.js
@@ -0,0 +1,17 @@
+/*
+when a client request there i middleware in between when user req the req send to the middleware and middleware can process the detail it check the detail if the prcess okay then to send forward 
+and if there is any reason recarding the securty and any other reason 
+then it send back the request
+
+execute on every call   
+it frward or end threq
+there is more than 1 middleware
+ 
+what it can do 
+execute code 
+make changes on code(res,req ) 
+call the next middleware 
+emd the req
+
+
+*/
